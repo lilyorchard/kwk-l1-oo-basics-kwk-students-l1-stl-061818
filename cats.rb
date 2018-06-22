8a1510f32af4790c98493b4e18c8d9c869192578
lilyorchard <lily@orchardfamily.com>
2018-06-22 15:31:39 +0000

    automatically backed up by learn

--- cats.rb
+++ cats.rb
@@ -6,6 +6,12 @@ class Cats
     @mean = mean
   end
   
+  def basic_facts
+    puts "this cat is an ovligatory carnivore"
+    puts "and it has curved claws"
+    puts "it has 30 teeth"
+  end
+  
   def is_it_cute
     @cute
   end
@@ -22,3 +28,5 @@ tiger = Cats.new("very","pretty fuzzy","super duper mean")
 garfield = Cats.new("has a bit of an attitude","up to debate","yes, he's hangry all the time")
 # puts "Is Garfield cute? #{garfield.is_it_cute}"
 puts "Is Garfield mean? #{garfield.is_it_mean}"
+
+puts "is tiger mean?" #{tiger.is_it_cute}"
