760c22aa704f7867d7e13eae469a137bf57162d0
lilyorchard <lily@orchardfamily.com>
2018-06-22 16:04:20 +0000

    automatically backed up by learn

--- shenanigans.rb
+++ shenanigans.rb
@@ -4,4 +4,29 @@ class Shenanigans
     @trait_2 = trait_2
     @trait_3 = trait_3
   end
-  lily = Shenanigans.new("has curly red hair", "has layered necklaces", "")
+ 
+def basic_similarities
+  puts "She is a girl"
+  puts "She is in high school"
+  puts "She is wearing a necklace and earrings"
+end
+  
+def hair
+  @trait_1
+end
+
+def necklaces
+  @trait_2
+end
+
+def earrings
+  @trait_3
+end
+  
+lily = Shenanigans.new("has curly red hair", "has layered necklaces", "has double pierced mismatch earrings")
+sam = Shenanigans.new("has straight black hair", "has a coin necklace", "has single pearl earrings")
+alice = Shenanigans.new("has curled brown hair", "has a locket necklace", "has single dangling earrings")
+jessica = Shenanigans.new("has straight black hair", "has a marble necklace", "has double pierced ball earrings")
+
+puts "What are some facts about Lily? #{.trait_1} #{.trait_2} #{.trait_3}"
+  
