a2f902d257681537529e3960726dabefca903aea
lilyorchard <lily@orchardfamily.com>
2018-06-22 15:54:28 +0000

    automatically backed up by learn

---
+++ shenanigans.rb
@@ -0,0 +1,7 @@
+class Shenanigans
+  def initialize(trait_1,trait_2,trait_3)
+    @trait_1 = trait_1
+    @trait_2 = trait_2
+    @trait_3 = trait_3
+  end
+  lily = Shenanigans.new("has curly red hair", "has layered necklaces", "")
