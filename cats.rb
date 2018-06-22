8a177f0c39a7c2ece3e97c518f7aa7e6e49c2f53
lilyorchard <lily@orchardfamily.com>
2018-06-22 15:32:38 +0000

    automatically backed up by learn

--- cats.rb
+++ cats.rb
@@ -29,4 +29,5 @@ garfield = Cats.new("has a bit of an attitude","up to debate","yes, he's hangry 
 # puts "Is Garfield cute? #{garfield.is_it_cute}"
 puts "Is Garfield mean? #{garfield.is_it_mean}"
 
-puts "is tiger mean?" #{tiger.is_it_cute}"
+puts "is tiger mean? #{tiger.is_it_mean}"
+puts "give me some basic facts about tiger. #{tiger.basic_facts}"
