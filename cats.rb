e95ba0ebdc369563f72fe273c69c49eb30934fde
lilyorchard <lily@orchardfamily.com>
2018-06-22 15:19:45 +0000

    automatically backed up by learn

--- cats.rb
+++ cats.rb
@@ -1,7 +1,19 @@
 class Cats
+  
   def intitialize(cute,fuzzy,mean)
     @cute = cute
     @fuzzy = fuzzy
     @mean = mean
   end
+  
+  def is_it_cute
+    @cute
+  end
+  
 end
+
+tiger = Cats.new("very","pretty fuzzy","super duper mean")
+puts "Is the tiger cute? #{tiger.is_it_cute}"
+
+garfield = Cats.new("has a bit of an attitude","up to debate","yes, he's hangry all the time")
+puts "Is Garfield cute? #{garfield.is_it_cute}"
